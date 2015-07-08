166830c0e5c5d72f491d73b565c5976a8a45c344
MCaste <mai_clarke_20@hotmail.com>
2015-07-07 23:08:36 -0300

    LAB03

---
+++ TP2L03/TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+
+namespace Business.Logic
+    {
+    public class UsuarioLogic : BusinessLogic 
+        {
+
+        UsuarioAdapter UA = new UsuarioAdapter();
+
+        public List<Usuario> GetAll()
+        {
+
+        return UA.GetAll();
+        }
+
+        public Business.Entities.Usuario GetOne(int ID) 
+      
+        {
+
+            return UA.GetOne(ID);
+        }
+
+
+        public void Delete( int ID )
+        {
+        UA.Delete(ID); 
+        }
+
+
+        public void Save( Usuario Usuario )
+        {
+
+        UA.Save(Usuario);
+            
+        }
+
+        }
+    }
