6c298c4463145c26f46b6b5ab974198fbac0c88d
mpintener <mpintener@gmail.com>
2015-09-13 12:04:32 -0300

    Otra parte
    leer, datos commit anterior

--- TP2L05/TP2/UI.Desktop/Plan.cs
+++ TP2L05/TP2/UI.Desktop/Plan.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
 
 namespace UI.Desktop
 {
-    public partial class Plan : Form
+    public partial class Plan : AplicationForm
     {
         public Plan()
         {
             InitializeComponent();
         }
+
+        public void Listar()
+            {
+                PlanLogic PL = new PlanLogic();
+
+            this.dgvPlan.DataSource = PL.GetAll();
+            }
+
+        private void Plan_Load( object sender, EventArgs e )
+            {
+            Listar();
+            }
+
+        private void btnActualizar_Click( object sender, EventArgs e )
+            {
+            Listar();
+            }
+
+        private void btnSalir_Click( object sender, EventArgs e )
+            {    
+            
+            DialogResult DR = (MessageBox.Show("Seguro que dese salir?", "Salir", MessageBoxButtons.YesNo));
+
+            if (DR == DialogResult.Yes) this.Close();        
+
+            }
+
+        private void tsbNuevo_Click( object sender, EventArgs e )
+            {
+            UsuarioDesktop UD = new UsuarioDesktop(AplicationForm.ModoForm.Alta);
+
+            UD.ShowDialog();
+
+            this.Listar();
+            }
+
+        private void tsbEditar_Click( object sender, EventArgs e )
+            {
+                if (this.dgvPlan.SelectedRows.Count != 0)
+                {
+
+                    int ID = ((Plan)this.dgvPlan.SelectedRows[0].DataBoundItem).ID;
+
+                    PlanDesktop UD = new PlanDesktop(ID, AplicationForm.ModoForm.Modificacion);
+
+                UD.ShowDialog();
+                }
+  
+            }
+
+        private void tsbEliminar_Click( object sender, EventArgs e )
+            {
+                if (this.dgvPlan.SelectedRows.Count != 0)
+                {
+
+                    int ID = ((Usuario)this.dgvPlan.SelectedRows[0].DataBoundItem).ID;
+
+                    PlanDesktop UD = new PlanDesktop(ID, AplicationForm.ModoForm.Baja);
+
+                UD.ShowDialog();
+                }
+            }
+        }
     }
-}
+
