6a1e9bda4ba7e87c6cf1ec6c19d44a715af8a03a
mpintener <mpintener@gmail.com>
2015-09-17 16:01:34 -0300

    ACa vaa otro
    :dancers:  :+1:  :jack_o_lantern:

--- TP2L05/TP2/Business.Entities/Usuario.cs
+++ TP2L05/TP2/Business.Entities/Usuario.cs
@@ -10,96 +10,46 @@ namespace Business.Entities
         {
 
         private string _NombreUsuario;
-
         private string _Clave;
-
         private string _Nombre;
-
         private string _Apellido;
-
         private string _Email;
-
         private bool _Habilitado;
 
 
         public string NombreUsuario 
         {
-
-        get { 
-            return _NombreUsuario;
-            }
-
-        set {
-            _NombreUsuario = value;
-            }
+        get { return _NombreUsuario; }
+        set { _NombreUsuario = value; }
         }
 
         public string Clave
-            {
-
-            get
-                {
-                return _Clave;
-                }
-
-            set
-                {
-                _Clave = value;
-                }
-            }
+        {
+            get { return _Clave; }
+            set { _Clave = value; }
+        }
 
         public string Nombre
-            {
-
-            get
-                {
-                return _Nombre;
-                }
-
-            set
-                {
-                _Nombre = value;
-                }
-            }
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+        
         public string Apellido
-            {
-
-            get
-                {
-                return _Apellido;
-                }
-
-            set
-                {
-                _Apellido= value;
-                }
-            }
+        {
+            get { return _Apellido; }
+            set { _Apellido = value; }
+        }
 
         public string Email
-            {
-
-            get
-                {
-                return _Email;
-                }
-
-            set
-                {
-                _Email = value;
-                }
-            }
+        {
+            get { return _Email; }
+            set { _Email = value; }
+        }
         public bool Habilitado
-            {
-
-            get
-                {
-                return _Habilitado;
-                }
-
-            set
-                {
-                _Habilitado = value;
-                }
-            }
+        {
+            get { return _Habilitado; }
+            set { _Habilitado = value; }
+        }
         }
     }
